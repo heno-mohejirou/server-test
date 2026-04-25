3fd44d83e2e423ef1b91c226a5f82aac017cf05d
へ野もへ次郎 <syousei.syousei.06.25@gmail.com>
2026-04-25 15:36:34 +0900

    Add files via upload

--- operation/browser_session.py
+++ operation/browser_session.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
 
 # ブラウザ操作クラス
@@ -22,17 +21,18 @@ class BrowserSession:
     # ブラウザ起動
     def boot(self):
         options = Options()
+
         options.binary_location = "/usr/bin/chromium"
 
-        options.add_argument("--headless")
-        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--headless=new")
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
 
         service = Service(executable_path="/usr/bin/chromedriver")  # ← ★これも重要
 
-        self.driver = webdriver.Chrome(service=service, options=options)
+        self.driver = webdriver.Chrome(
+            service=service, options=options)
 
         self.driver.get(self.url)
 
